103ae5f0ed8ce24e496134bc15e7bfd26b181df5
Sean Klechak <sklechak@students.mchenry.edu>
2018-08-28 16:16:42 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,46 @@
+// ********************************************************************************************************************************************************************
+//                          	Use the following types in your program:
+//
+//     	                     Primitive Data Types:
+//
+//                          	byte 8-bit signed integer. Can be used in place of int to save memory.
+//                          	short - 16-bit signed integer. Can be used in place of int to save memory.
+//                          	int -32 bit signed integer. Has a supporting Integer class (Links to an external site.)Links to an external site.
+//                          	long-64 bit integer
+//                              float-Single-precision floating point (small number with a decimal point)
+//                          	double-64 bit floating point (large number with a decimal point)
+//                              boolean-true / false values
+//                              char-single 16-bit Unicode Character
+//                          	Create pseudocode and hand in either in a Word document or by typing into the text box
+//                          	Upload to GitHub and hand in a link directly to the Main file
+//
+// **********************************************************************************************************************************************************************
+
+public class main {
+
+    public static void main(String[] args) {
+
+        byte bByte = 8;
+        short sShort = 10;
+        int iINt = 12;
+        long lLong = 81012;
+        float fFloat = 81.0182f;
+        double dDouble = 81.029029d;
+        boolean bBoolean = true;
+        char cChar = 'L';
+
+        System.out.println("byte = " + bByte.);
+        System.out.println("short = " + sShort);
+        System.out.println("int = " + iINt);
+        System.out.println("long = " + lLong);
+        System.out.println("float = " + fFloat);
+        System.out.println( "double = " + dDouble);
+        System.out.println("Boolean = " + bBoolean);
+        System.out.println("char = " + cChar);
+
+
+
+
+    }
+}
+
